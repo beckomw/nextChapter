c7f8ada5fe873d4fc6cd36285ed7ae741dddc933
Wayne Beckom <Waynebeckom@gmail.com>
2024-05-23 03:23:32 -0600

    new files

---
+++ hack-reactor/explorations/python-programs/dictionary_exercises.py
@@ -0,0 +1,22 @@
+
+
+# continents_countries = {
+#   "Asia":["China", "Mongolia", "India"],
+#   "South America": ["Brazil", "Argentina", "Chile"],
+#   "North America": ["United States", "Canada", "Mexico"], "Antarctica": [],
+#   "Africa": ["South Africa", "Algeria", "Kenya", "Ethiopia", "Egypt"],
+#   "Europe": ["France", "Germany", "England", "Spain", "Greece", "Italy"],
+#   "Australia": ["New Zealand", "Australia", "Fiji"]
+#   }
+
+
+# print(continents_countries["Asia"])
+
+# print(continents_countries["Australia"][2])
+
+# keys = continents_countries.keys()
+# sorted_continents = sorted(keys)
+
+# print(sorted_continents)
+
+
---
+++ hack-reactor/explorations/python-programs/list_exercises.py
@@ -0,0 +1,12 @@
+# list1 = [100,200,300,400,500]
+
+# list1.reverse()
+# print(list1)
+
+numbers = [1,2,3,4,5,6,7]
+
+for i in range(len(numbers)):
+    numbers[i] = numbers[i] ** 2
+
+
+print(numbers)
